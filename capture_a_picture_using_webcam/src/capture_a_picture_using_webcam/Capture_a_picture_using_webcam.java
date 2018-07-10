279026695a0e7e34abfb8420dfda2754dacaf026
Nayanajith <nayanajithpriyasad@gmail.com>
2018-07-11 00:12:10 +0530

    capture_a_picture_using_webcam

---
+++ capture_a_picture_using_webcam/src/capture_a_picture_using_webcam/Capture_a_picture_using_webcam.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capture_a_picture_using_webcam;
+
+/**
+ *
+ * @author Nayanajith Priyasad
+ */
+public class Capture_a_picture_using_webcam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
